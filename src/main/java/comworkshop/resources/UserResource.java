a5a1a72bc2e9401f763be7ed7b3ccfff620c5e97
Matheus C <matheus.caposse02@gmail.com>
2026-02-14 18:44:32 -0300

    Obtendo usuario por ID e capturando excessao

--- src/main/java/comworkshop/resources/UserResource.java
+++ src/main/java/comworkshop/resources/UserResource.java
@@ -5,10 +5,7 @@ import comworkshop.domain.User;
 import comworkshop.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,5 +26,11 @@ public class UserResource {
                 .map(x-> new UserDTO(x))
                 .collect(Collectors.toList());
         return ResponseEntity.ok().body(listDTO);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(user));
     }
 }
--- src/main/java/comworkshop/service/UserService.java
+++ src/main/java/comworkshop/service/UserService.java
@@ -2,11 +2,13 @@ package comworkshop.service;
 
 import comworkshop.domain.User;
 import comworkshop.repository.UserRepository;
+import comworkshop.service.exception.ObjectNotFoundException;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
     public List<User> findAll(){
         return userRepository.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> user = userRepository.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Error! user not found"));
     }
 }
