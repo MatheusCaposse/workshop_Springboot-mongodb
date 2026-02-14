b6b11035fd56989b59e5842168527c3a3db4791b
Matheus C <matheus.caposse02@gmail.com>
2026-02-14 14:28:19 -0300

    implementando padrao DTO para retornar usuarios

--- src/main/java/comworkshop/resources/UserResource.java
+++ src/main/java/comworkshop/resources/UserResource.java
@@ -1,5 +1,6 @@
 package comworkshop.resources;
 
+import comworkshop.DTO.UserDTO;
 import comworkshop.domain.User;
 import comworkshop.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,8 +22,12 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDTO = list
+                .stream()
+                .map(x-> new UserDTO(x))
+                .collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
