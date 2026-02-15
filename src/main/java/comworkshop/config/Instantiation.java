bb63358df71dd4c1f20df050e771a0160d368418
Matheus C <matheus.caposse02@gmail.com>
2026-02-15 13:10:55 -0300

    crianda Post e PostRepository

--- src/main/java/comworkshop/config/Instantiation.java
+++ src/main/java/comworkshop/config/Instantiation.java
@@ -1,30 +1,41 @@
 package comworkshop.config;
 
+import comworkshop.domain.Post;
 import comworkshop.domain.User;
+import comworkshop.repository.PostRepository;
 import comworkshop.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.ArrayList;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
     @Autowired
-    UserRepository repository;
+    UserRepository userRepository;
+    @Autowired
+    PostRepository postRepository;
+
 
     @Override
     public void run(String... args) throws Exception {
 
-        repository.deleteAll();
+        userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown" ,"maria@gmail.com");
         User alex = new User(null, "Alex Green" ,"alex@gmail.com");
         User bob = new User(null, "Bob Chawnton" ,"bob@yahoo.com");
 
-        repository.saveAll(Arrays.asList(maria, alex, bob));
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem!", "Vou viajar para São Paulo", maria);
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz", maria);
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
