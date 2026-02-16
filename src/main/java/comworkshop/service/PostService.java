1095a84d66540b0ed9b9062d103e323ef3293ee6
Matheus C <matheus.caposse02@gmail.com>
2026-02-16 13:55:04 -0300

    criando endpoint para busca de post por titulo utilizando QUERY METHODS

--- src/main/java/comworkshop/service/PostService.java
+++ src/main/java/comworkshop/service/PostService.java
@@ -22,4 +22,8 @@ public class PostService {
         Optional<Post> post = postRepository.findById(id);
         return post;
     }
+
+    public List<Post> findByTitle(String text){
+        return postRepository.findByTitleContainingIgnoreCase(text);
+    }
 }
